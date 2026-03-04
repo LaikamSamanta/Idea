0b35ba439edbc69fdc781f3a7d96f349da7f30e0
Samanta Biezēka <sbiezeka@gmail.com>
2026-03-04 09:20:23 +0200

    Tested tailwind

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -5,9 +5,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Idea</title>
-    @vite(['resources/css/app.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-background text-foreground">
   <p>Hello World</p>
+  <div class="bg-red-500 text-white text-5xl p-10">
+    TAILWIND TEST
+</div>
 </body>
 </html>
